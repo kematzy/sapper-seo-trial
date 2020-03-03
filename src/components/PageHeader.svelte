7f557221683da92fb6f3b02111dc07bdeeff0d82
Kematzy <kematzy@gmail.com>
2020-03-03 09:05:01 +0800

    Add PageHeader component

---
+++ src/components/PageHeader.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let dpage
+</script>
+
+<h1>{dpage.title || 'missing title'}</h1>
+<p><strong>Note!</strong> this <code>PageHeader</code> is not repating when navigating between the pages</p>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
