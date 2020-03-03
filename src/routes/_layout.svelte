c9d3ce7a9b2b8850b86a0309160bb076c34d5175
Kematzy <kematzy@gmail.com>
2020-03-03 09:04:45 +0800

    Add Example info

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -13,10 +13,25 @@
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
+
+	h3 {
+		font-weight: bold;
+		color: #777;
+	}
 </style>
 
 <Nav {segment}/>
 
 <main>
 	<slot></slot>
+
+	<div style="margin-top: 4rem; border-top: 1px solid #ccc; padding: 1rem 0;">
+		<h3>About This Example</h3>
+		<p>This example explores how to DRY up the whole SEO tags generation on pages.</p>
+		<p>
+			For some reason, the <code>&lt;SeoTags /&gt;</code> component within the <code>&lt;svelte:head&gt;</code>
+			<strong>causes the tag output to repeat on each page visit, causing multiple copies of the output in <code>&lt;head&gt;</code> </strong>.
+		</p>
+		<p>Inspect HTML output.</p>
+	</div>
 </main>
