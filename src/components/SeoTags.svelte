b286efec826f5a31938e0003ce904d0341f78fb6
Kematzy <kematzy@gmail.com>
2020-03-03 08:44:39 +0800

    Add SeoTags component

---
+++ src/components/SeoTags.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let dpage = {
+    title: "Default Page Title",
+    titleSuffix: " | Default Title Suffix",
+    description: "Default Page Description",
+  }
+</script>
+
+<!-- BEGIN SEO -->
+<title>{dpage.title + dpage.titleSuffix}</title>
+<meta name="description" content="{dpage.description}" />
+
+<meta property="og:title" content="{dpage.title}" />
+<meta property="og:description" content="{dpage.description}" />
+
+<meta property="twitter:title" content="{dpage.title}" />
+<meta property="twitter:description" content="{dpage.description}" />
+<!-- /END SEO -->
