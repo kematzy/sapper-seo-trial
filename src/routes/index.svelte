59377578abd56a5cb18366bda02675da3adc030a
Kematzy <kematzy@gmail.com>
2020-03-04 14:09:11 +0800

    Update Home page contents

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,6 @@
 <style>
-  h2, figure, p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h2 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
   figure {
+    text-align: center;
     margin: 0 0 1em 0;
   }
 
@@ -22,13 +11,7 @@
   }
 
   p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h2 {
-      font-size: 4em;
-    }
+    margin: 1rem 1rem 3rem 0;
   }
 </style>
 
@@ -45,11 +28,9 @@
 
 <PageHeader dpage="{$seo}" />
 
-<h2>Sapper SEO stores version</h2>
+<p>DRY-ing up SEO tags generation in Sapper / Svelte apps</p>
 
 <figure>
   <img alt='Borat' src='great-success.png'>
-  <figcaption>HIGH FIVE!</figcaption>
+  <figcaption>IT WORKS SOMEHOW!</figcaption>
 </figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
