01537ac08f0d808b96a43ddf33ba80d4c79ef72d
Kematzy <kematzy@gmail.com>
2020-03-04 13:05:36 +0800

    Update SeoTags.svelte
    Add additional SEO tags and data from the store

--- src/components/SeoTags.svelte
+++ src/components/SeoTags.svelte
@@ -1,18 +1,27 @@
 <script>
-  export let dpage = {
-    title: "Default Page Title",
-    titleSuffix: " | Default Title Suffix",
-    description: "Default Page Description",
-  }
+  import { seo } from '../stores.js'
 </script>
 
 <!-- BEGIN SEO -->
-<title>{dpage.title + dpage.titleSuffix}</title>
-<meta name="description" content="{dpage.description}" />
+<title>{$seo.title} | {$seo.titleSuffix}</title>
+<meta name="description" content="{$seo.description}" />
 
-<meta property="og:title" content="{dpage.title}" />
-<meta property="og:description" content="{dpage.description}" />
+<meta property="og:title" content="{$seo.title}" />
+<meta property="og:description" content="{$seo.description}" />
+<meta property="og:image" content="{$seo.image.src}" />
+<meta property="og:image:alt" content="{$seo.image.alt}" />
+<meta property="og:image:width" content="{$seo.image.width}"/>
+<meta property="og:image:height" content="{$seo.image.height}" />
+<meta property="og:site_name" content="{$seo.siteName}" />
+<meta property="og:type" content="{$seo.ogType}" />
+<meta property="og:locale" content="{$seo.siteLocale}" />
 
-<meta property="twitter:title" content="{dpage.title}" />
-<meta property="twitter:description" content="{dpage.description}" />
+<meta property="twitter:title" content="{$seo.title}" />
+<meta property="twitter:description" content="{$seo.description}" />
+<meta name="twitter:title" content="{$seo.title}" />
+<meta name="twitter:description" content="{$seo.description}" />
+<meta property="twitter:image" content="{$seo.image.src}" />
+<meta property="twitter:image:alt" content="{$seo.image.alt}" />
+<meta name="twitter:card" content="{$seo.twitterCard || 'summary'}" />
+<meta name="twitter:site" content="{$seo.twitter}" />
 <!-- /END SEO -->
