76e850ca4b0d5b5ade468b5357b84b6a88763258
Kematzy <kematzy@gmail.com>
2020-03-04 13:18:26 +0800

    Remove unused text

--- src/components/PageHeader.svelte
+++ src/components/PageHeader.svelte
@@ -3,10 +3,9 @@
 </script>
 
 <h1>{dpage.title || 'missing title'}</h1>
-<p><strong>Note!</strong> this <code>PageHeader</code> is not repating when navigating between the pages</p>
 
 <style>
   h1 {
-    color: red;
+    color: #159794;
   }
 </style>
