64f74a0e905e1ad612d2a950d00265f64784a99c
Kematzy <kematzy@gmail.com>
2020-03-05 13:13:56 +0800

    Updated Cypress specs

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,19 +1,78 @@
 describe('Sapper template app', () => {
-	beforeEach(() => {
-		cy.visit('/')
+
+	describe('Home page', () => {
+		beforeEach(() => {
+			cy.visit('/');
+		});
+	
+		it('has the correct <title>', () => {
+			cy.title().should('eq', 'Sapper SEO Trial | Sapper SEO Trial [Settings]');
+		});
+
+		it('has the correct <meta description>', () => {
+			cy.get('head meta[name="description"]')
+				.should("have.attr", "content", "Home page description");
+		});
+
+		it('has the correct <h1>', () => {
+			cy.contains('h1', 'Sapper SEO Trial');
+		});
 	});
 
-	it('has the correct <h1>', () => {
-		cy.contains('h1', 'Great success!')
+	describe('About page', () => {
+		beforeEach(() => {
+			cy.visit('/about');
+		});
+	
+		it('has the correct <title>', () => {
+			cy.title().should('eq', 'About | Sapper SEO Trial [Settings]');
+		});
+
+		it('has the correct <meta description>', () => {
+			cy.get('head meta[name="description"]')
+				.should("have.attr", "content", "About page description");
+		});
+
+		it('has the correct <h1>', () => {
+			cy.contains('h1', 'About');
+		});
 	});
 
-	it('navigates to /about', () => {
-		cy.get('nav a').contains('about').click();
-		cy.url().should('include', '/about');
+	describe('Blog page', () => {
+		beforeEach(() => {
+			cy.visit('/blog');
+		});
+	
+		it('has the correct <title>', () => {
+			cy.title().should('eq', 'Blog | Sapper SEO Trial [Settings]');
+		});
+
+		it('has the correct <meta description>', () => {
+			cy.get('head meta[name="description"]')
+				.should("have.attr", "content", "Blog page description");
+		});
+
+		it('has the correct <h1>', () => {
+			cy.contains('h1', 'Blog');
+		});
 	});
 
-	it('navigates to /blog', () => {
-		cy.get('nav a').contains('blog').click();
-		cy.url().should('include', '/blog');
+	describe('Blog Sub page', () => {
+		beforeEach(() => {
+			cy.visit('/blog/what-is-sapper');
+		});
+	
+		it('has the correct <title>', () => {
+			cy.title().should('eq', 'What is Sapper? | Sapper SEO Trial [Settings]');
+		});
+
+		it('has the correct <meta description>', () => {
+			cy.get('head meta[name="description"]')
+				.should("have.attr", "content", "Blog page description");
+		});
+
+		it('has the correct <h1>', () => {
+			cy.contains('h1', 'What is Sapper?');
+		});
 	});
 });
